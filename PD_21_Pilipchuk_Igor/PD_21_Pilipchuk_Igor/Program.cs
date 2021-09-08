b6af9f116af07e1d729acd5e11af011bf64bec2d
Igor <pilipchuk.rus.mal.ip@gmail.com>
2021-09-08 19:30:22 +0300

    Lab1+2

---
+++ PD_21_Pilipchuk_Igor/PD_21_Pilipchuk_Igor/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD_21_Pilipchuk_Igor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
